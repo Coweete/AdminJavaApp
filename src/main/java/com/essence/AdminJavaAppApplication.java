d42492666e0ff68ea522a6bb8a4cf6392b8cef1a
Coweete <Jonatan_fridsten@hotmail.com>
2016-04-20 11:21:46 +0200

    Initial commit

---
+++ src/main/java/com/essence/AdminJavaAppApplication.java
@@ -0,0 +1,12 @@
+package com.essence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminJavaAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/essence/AdminJavaAppApplicationTests.java
@@ -0,0 +1,18 @@
+package com.essence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AdminJavaAppApplication.class)
+@WebAppConfiguration
+public class AdminJavaAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
